7f2f13bc8c680670b34fa41ad2e318a62131e169
Goose <Hus.O.Y@nmu.one>
2023-04-11 23:37:07 +0300

    add task_2

---
+++ Task_2/main.js
@@ -0,0 +1,33 @@
+const fs = require('fs');
+const inputData = require('./input.json');
+
+let results = [];
+
+inputData.forEach(arr => {
+
+    const n = arr.length;
+    
+    const frequency = new Array(n + 1).fill(0);
+
+    const result = {
+        arr: arr,
+        repetitive: 0
+    };
+
+	for (let i = 0; i < n; i++) {
+		frequency[arr[i]]++;
+	};
+
+	for (let i = 0; i < n; i++) {
+
+		if (frequency[arr[i]] > 1) {
+
+            result.repetitive = arr[i];
+            results.push(result);
+
+			break;
+		};
+	};
+});
+
+fs.writeFileSync('output.json', JSON.stringify(results));
