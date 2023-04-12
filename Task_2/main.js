acdbfb4c3d8328229033ed8b4073a7efd212eab3
Goose <Hus.O.Y@nmu.one>
2023-04-12 14:54:33 +0300

    add task_2

--- Task_2/main.js
+++ Task_2/main.js
@@ -6,7 +6,7 @@ let results = [];
 inputData.forEach(arr => {
 
     const n = arr.length;
-    
+
     const frequency = new Array(n + 1).fill(0);
 
     const result = {
@@ -14,20 +14,20 @@ inputData.forEach(arr => {
         repetitive: 0
     };
 
-	for (let i = 0; i < n; i++) {
-		frequency[arr[i]]++;
-	};
+    for (let i = 0; i < n; i++) {
+        frequency[arr[i]]++;
+    };
 
-	for (let i = 0; i < n; i++) {
+    for (let i = 0; i < n; i++) {
 
-		if (frequency[arr[i]] > 1) {
+        if (frequency[arr[i]] > 1) {
 
             result.repetitive = arr[i];
             results.push(result);
 
-			break;
-		};
-	};
+            break;
+        };
+    };
 });
 
 fs.writeFileSync('output.json', JSON.stringify(results));
