678eda7ce15f0ac97456323c851b993f876bba1c
Goose <Hus.O.Y@nmu.one>
2023-04-17 12:00:06 +0300

    add final refactoring

--- Task_3.1/main.js
+++ Task_3.1/main.js
@@ -7,19 +7,21 @@ const combinedLoaders = loaders.concat(lbsLoaders).sort((a, b) => a - b);
 
 let results = [];
 
-inputData.forEach(item => {
+inputData.forEach(item => { 
+
     let currentWeight = 20;
     let remainingWeight = item - currentWeight;
     let remainingSpace = 12;
     const variants = [];
     const loadersCopy = [...combinedLoaders];
 
-    while(loadersCopy) {
-        for (let i = loadersCopy.length - 1; i >= 0; i--) {
-            while (remainingWeight >= loadersCopy[i] * 2) {
-                if (remainingSpace === 0) break;
+    for (let k = 1; k <= loadersCopy.length; k++) {
 
-                currentWeight += loadersCopy[i] * 2;
+        for (let i = loadersCopy.length - 1; i >= 0; i--) {
+
+            while (remainingWeight + loaders[0] >= loadersCopy[i] * 2) {
+                if (remainingSpace === 0) { break }
+                currentWeight += loadersCopy[i] * 2
                 remainingWeight = item - currentWeight;
                 remainingSpace -= 1;
             };
@@ -30,11 +32,9 @@ inputData.forEach(item => {
         remainingWeight = item - currentWeight;
         loadersCopy.splice(loadersCopy.length - 1, 1);
     };
-
     const filteredVariants = variants.filter(variant => variant > item);
     const result = Math.min(...filteredVariants);
     results.push(result);    
 });
 
 fs.writeFileSync('output.json', JSON.stringify(results));
-
